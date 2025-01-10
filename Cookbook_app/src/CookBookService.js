a9a500f80a71390cb916897500188ce3a1c3866d
milestarricone <milestarricone2@gmail.com>
2025-01-10 15:05:34 -0500

    added create and update functions to service module.

--- Cookbook_app/src/CookBookService.js
+++ Cookbook_app/src/CookBookService.js
@@ -1,38 +1,16 @@
-const DjangoAPI = 'http://34.228.156.43:8000/api/recipe';
-
-// Get request
-export const getRecipesFromDjango = async () => {
-    try {
-        const response = await fetch(DjangoAPI);
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const data = await response.json();
-        // console.log(data);
-        return data;
-    } catch (error) {
-        console.error("Failed to fetch recipes:", error.message);
-        return { error: error.message };
-    }
-};
-
-//update request
-// export const updateRecipesFromDjango = async (id, bodyInfo) => {
-//     try {
-//         const updatedRecipe = 
-//     } catch (error) {
-        
-//     }
-// }
+const DjangoAPI = "http://34.228.156.43:8000/api/recipe";
 
 
 
-
-// Delete request
-export const deleteRecipesFromDjango = async () => {
+// Create request
+export const createRecipeFromDjango = async (bodyInfo) => {
     try {
         const response = await fetch(DjangoAPI, {
-            method: 'DELETE',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(bodyInfo),
         });
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
@@ -41,7 +19,64 @@ export const deleteRecipesFromDjango = async () => {
         console.log(data);
         return data;
     } catch (error) {
-        console.error('Failed to delete recipes:', error.message);
+        console.error("Failed to create recipe:", error.message);
         return { error: error.message };
     }
+};
+
+
+
+// Get request
+export const getRecipesFromDjango = async () => {
+  try {
+    const response = await fetch(DjangoAPI);
+    if (!response.  ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    // console.log(data);
+    return data;
+  } catch (error) {
+    console.error("Failed to fetch recipes:", error.message);
+    return { error: error.message };
+  }
+};
+
+// update request
+export const updateRecipesFromDjango = async (id, bodyInfo) => {
+  try {
+    const response = await fetch(`${DjangoAPI}/${id}`, {
+      method: "PUT",
+    //   headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(bodyInfo),
+    });
+    if (!response.ok) {
+        throw new Error(`HTTP Error! Status: ${response.status}`)
+    }
+    const data = await response.json()
+    console.log(data)
+    return data
+  } catch (error) {
+    console.error("Failed to update", error.message);
+    return { error: error.message}
+    
+  }
+};
+
+// Delete request
+export const deleteRecipesFromDjango = async (id) => {
+  try {
+    const response = await fetch(`${DjangoAPI}/${id}`, {
+      method: "DELETE",
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error("Failed to delete recipes:", error.message);
+    return { error: error.message };
+  }
 };
