4a723809e1ed72fd3d0b03a11cdcfe108341ba3f
Taihlor Elliott <taihlor.elliott@gmail.com>
2025-01-14 11:47:48 -0500

    bootstrap update page

--- Cookbook_app/src/components/Footer.jsx
+++ Cookbook_app/src/components/Footer.jsx
@@ -2,26 +2,26 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer style={styles.footer}>
-      <p style={styles.text}>© {new Date().getFullYear()} My Company. All rights reserved.</p>
+    <footer className="bg-dark fixed-bottom">
+      <p className="text-light m-auto text-center">© {new Date().getFullYear()} The Cookbook. All rights reserved.</p>
     </footer>
   );
 }
 
-const styles = {
-  footer: {
-    backgroundColor: 'blue',
-    color: 'white',
-    padding: '10px 20px',
-    textAlign: 'center',
-    position: 'fixed',
-    bottom: 0,
-    width: '100%',
-  },
-  text: {
-    margin: 0,
-    fontSize: '14px',
-  },
-};
+// const styles = {
+//   footer: {
+//     backgroundColor: 'blue',
+//     color: 'white',
+//     padding: '10px 20px',
+//     textAlign: 'center',
+//     position: 'fixed',
+//     bottom: 0,
+//     width: '100%',
+//   },
+//   text: {
+//     margin: 0,
+//     fontSize: '14px',
+//   },
+// };
 
 export default Footer;
